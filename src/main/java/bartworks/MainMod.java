e857bf9c5eec8cd9de3ebbaa85ec041a5739b12a
SST-03 <70825063+SST-03@users.noreply.github.com>
2025-09-07 18:14:17 +0800

    revert

--- src/main/java/bartworks/MainMod.java
+++ src/main/java/bartworks/MainMod.java
@@ -68,7 +68,6 @@ import gregtech.api.enums.Mods;
 import gregtech.api.recipe.RecipeMap;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
 import gregtech.api.util.GlassTier;
-import gtneioreplugin.util.CSVMaker;
 import tectech.loader.recipe.Godforge;
 
 @Mod(
@@ -190,7 +189,6 @@ public final class MainMod {
         runOnServerStarted();
         StaticRecipeChangeLoaders.unificationRecipeEnforcer();
         VoidMinerUtility.generateDropMaps();
-        new CSVMaker().runVoidMiner();
     }
 
     private static boolean recipesAdded;
